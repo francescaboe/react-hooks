bd445f17e423a6a533bd253761c3b49a5298bb03
francescaboe <francesca.boe.ac@gmail.com>
2023-12-06 13:22:35 +0100

    complete ex 6.8

---
+++ src/exercise/06-extra8.js
@@ -0,0 +1,99 @@
+// useEffect: HTTP requests
+// http://localhost:3000/isolated/exercise/06.js
+
+import * as React from 'react'
+import { ErrorBoundary } from "react-error-boundary"
+import { PokemonForm, fetchPokemon,PokemonInfoFallback, PokemonDataView } from '../pokemon'
+
+const STATUS = {
+  IDLE: 'dle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected'
+}
+
+function fallbackRender({ error, resetErrorBoundary }) {
+
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{ color: "red" }}>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  );
+}
+
+function PokemonInfo({pokemonName}) {
+
+  const [{status, pokemon, error}, setState] = React.useState({
+    status: pokemonName ? STATUS.PENDING: STATUS.IDLE,
+    pokemon: null,
+  })
+
+  React.useEffect(()=> {
+    if(!pokemonName){
+      return
+    }
+
+    setState((prevState) => ({
+      ...prevState,
+      status: STATUS.PENDING,
+    }))
+
+    fetchPokemon(pokemonName).then(
+      pokemon => {
+        setState((prevState) => ({
+          ...prevState,
+          status: STATUS.RESOLVED,
+          pokemon
+        }));
+      },
+      error => {
+        setState((prevState) => ({
+          ...prevState,
+          status: STATUS.REJECTED,
+          error
+        }));
+      },
+    )
+
+  },[pokemonName])
+
+  if(status === STATUS.REJECTED){
+    // lets ErrorBoundary deal with the error
+    throw error
+  }
+
+  if(status === STATUS.IDLE) {
+    return 'Submit a pokemon'
+  }
+
+  if(status === STATUS.PENDING){
+    return <PokemonInfoFallback name={pokemonName} />
+  }
+  return <PokemonDataView pokemon={pokemon} />
+}
+
+function App() {
+  const [pokemonName, setPokemonName] = React.useState('')
+
+  function handleSubmit(newPokemonName) {
+    setPokemonName(newPokemonName)
+  }
+
+  return (
+    <div className="pokemon-info-app">
+      <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+      <hr />
+      <div className="pokemon-info">
+        {/*by adding a key prop, we allow the ErrorBoundary's state to reset, so that on pokemonName change the error becomes null again, rerender is triggered*/}
+        {/*but the key prop would trigger the render of the boundary every time it changes, causing Pokemon info to reredner every time, even on new pokemon submit*/}
+        <ErrorBoundary fallbackRender={fallbackRender} onReset={()=>setPokemonName('')} resetKeys={[pokemonName]} >
+          <PokemonInfo pokemonName={pokemonName} />
+        </ErrorBoundary>
+      </div>
+    </div>
+  )
+}
+
+export default App
